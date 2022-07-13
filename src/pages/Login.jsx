61f3fe7b994c40fc674482424c7f708e0d1eb320
luhluh-17 <castro.ralph17@gmail.com>
2022-07-14 00:45:43 +0800

    authenticate user login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,76 @@
-import React from 'react'
+import { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import InputField from '../components/InputField'
+import api, { LOGIN } from '../services/api'
 
 function Login() {
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  const [error, setError] = useState(`We're so excited to see you again!`)
+
   const navigate = useNavigate()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const data = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    }
+
+    api
+      .post(LOGIN, data)
+      .then(result => {
+        const {
+          data: { id },
+        } = result.data
+
+        const {
+          'access-token': accessToken,
+          client,
+          expiry,
+          uid,
+        } = result.headers
+
+        localStorage.setItem('acces-token', accessToken)
+        localStorage.setItem('client', client)
+        localStorage.setItem('expiry', expiry)
+        localStorage.setItem('uid', uid)
+        localStorage.setItem('id', id)
+
+        navigate('/home')
+      })
+      .catch(error => {
+        const { errors } = error.response.data
+        setError(errors[0])
+      })
+  }
 
   const handleClick = () => navigate('/register')
 
   return (
     <main>
       <section>
-        <header>
-          <h3>Welcome Back</h3>
-          <p>We're so excited to see you again!</p>
-        </header>
-        <form>
-          <InputField label={'email'} />
-          <InputField label={'password'} type={'password'} />
+        <form onSubmit={handleSubmit}>
+          <header>
+            <h3>Welcome Back</h3>
+            <p>{error}</p>
+          </header>
+          <label className='form-label'>
+            Email
+            <input className='form-input' type='email' ref={emailRef} />
+          </label>
+          <label className='form-label'>
+            Password
+            <input
+              className='form-input'
+              type='password'
+              autoComplete='on'
+              ref={passwordRef}
+            />
+          </label>
           <p>Forgot your password?</p>
-          <button type='submit'>Login</button>
+          <button>Login</button>
           <p>Need an account?</p>
           <button onClick={handleClick}>Register</button>
         </form>
--- src/services/api.js
+++ src/services/api.js
@@ -2,41 +2,12 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: 'http://206.189.91.54/api/v1',
+  headers: { 'Content-Type': 'application/json' },
 })
 
-const REGISTER = '/auth'
-const LOGIN = '/auth/sign_in'
-const USERS = '/users'
-const MESSAGES = '/messages'
-const CHANNELS = '/channels'
-const ADD_MEMBER = '/channel/add_member'
-
-const MESSAGES_PARAMS = (receiver_id, receiver_class) => {
-  const PARAMS = `receiver_id=${receiver_id}&receiver_class=${receiver_class}`
-  return `${MESSAGES}?${PARAMS}`
-}
-
-const CHANNEL_DETAILS = channel_id => {
-  return `${CHANNELS}/${channel_id}`
-}
-
-// export const registerAccount = (email, password, password_confirmation) => {
-//   const api = `${BASE_URL}/auth`
-
-//   const data = JSON.stringify({
-//     email,
-//     password,
-//     password_confirmation,
-//   })
-
-//   const config = {
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   }
-
-//   axios
-//     .post(api, data, config)
-//     .then(result => console.log(result))
-//     .catch(error => console.log(error))
-// }
+export const REGISTER = '/auth'
+export const LOGIN = '/auth/sign_in'
+export const USERS = '/users'
+export const MESSAGES = '/messages'
+export const CHANNELS = '/channels'
+export const ADD_MEMBER = '/channel/add_member'
