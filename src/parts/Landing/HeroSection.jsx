9864610d72ee965ecc88bfefa1a12a3dbdc2d138
luhluh-17 <castro.ralph17@gmail.com>
2022-07-15 00:35:21 +0800

    add styles to hero section

--- src/parts/Landing/HeroSection.jsx
+++ src/parts/Landing/HeroSection.jsx
@@ -9,18 +9,24 @@ function HeroSection() {
   return (
     <section className='hero-section'>
       <div className='hero-navbar'>
-        <h2>Logo</h2>
-        <button onClick={handleLogin}>Login</button>
+        <h2>Slack Clone</h2>
+        <button className='btn-round' onClick={handleLogin}>
+          Login
+        </button>
       </div>
-      <div className='hero-container'>
-        <h1>Imagine a place...</h1>
-        <h3>
-          ...where you can belong to a school club, a gaming group, or a
-          worldwide art community. Where just you and a handful of friends can
-          spend time together. A place that makes it easy to talk every day and
-          hang out more often.
-        </h3>
-        <button onClick={handleRegister}>Register</button>
+      <div className='flex-col'>
+        <div className='hero-container'>
+          <h1>Imagine a place...</h1>
+          <h3>
+            ...where you can belong to a school club, a gaming group, or a
+            worldwide art community. Where just you and a handful of friends can
+            spend time together. A place that makes it easy to talk every day
+            and hang out more often.
+          </h3>
+        </div>
+        <button className='btn-round-dark' onClick={handleRegister}>
+          Explore the Community
+        </button>
       </div>
       <div></div>
     </section>
