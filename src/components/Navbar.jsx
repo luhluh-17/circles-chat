48fac1b57ec407b364b17602fc4152f365130391
luhluh-17 <castro.ralph17@gmail.com>
2022-07-18 20:05:24 +0800

    adds navigation to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { getInitials } from '../utils/helper'
 import Searchbar from './Searchbar'
 
 function Navbar() {
+  const navigate = useNavigate()
+
+  const navigateToHome = () => {
+    navigate('/home')
+  }
+
+  const navigateToMessages = () => {
+    navigate('/messages')
+    localStorage.clear()
+  }
+
+  const handleLogout = () => {
+    navigate('/')
+    localStorage.clear()
+  }
+
   return (
     <nav className='navbar'>
       <Searchbar />
       <ul className='navbar-items'>
-        <li>
+        <li onClick={navigateToHome}>
           <span className='material-symbols-outlined icon'>home</span>
         </li>
-        <li>
+        <li onClick={navigateToMessages}>
           <span className='material-symbols-outlined icon'>mail</span>
         </li>
-        <li>
+        <li onClick={handleLogout}>
           <span className='dot'>{getInitials()}</span>
         </li>
       </ul>
