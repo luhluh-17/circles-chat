bc49ea75c144446e7d21fdd4bb2361405b06d8e8
luhluh-17 <castro.ralph17@gmail.com>
2022-07-22 21:03:12 +0800

    add modal to create new channels

--- src/parts/Channel/ModalAddChannel.jsx
+++ src/parts/Channel/ModalAddChannel.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ControlledInput from '../../components/ControlledInput'
 import FormButton from '../../components/FormButton'
 import FormHeader from '../../components/FormHeader'
 import Modal from '../../components/Modal'
-import UncontrolledInput from '../../components/UncontrolledInput'
+import { apiPOST } from '../../services/api'
+import { CHANNELS } from '../../services/constant'
+import { validateChannel } from '../../utils/validators'
 
 function ModalAddChannel({ isOpen, onClose }) {
+  const [channel, setChannel] = useState('')
+  const [error, setError] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if (validateChannel(channel).result) {
+      const data = {
+        name: channel,
+        user_ids: [localStorage.getItem('id')],
+      }
+
+      const onSuccess = response => {
+        if (response.data?.errors === undefined) {
+          onClose()
+          setChannel('')
+        } else {
+          setError('has already been taken')
+        }
+      }
+
+      const onError = error => {
+        console.log('ChannelAddError', error)
+      }
+
+      apiPOST(CHANNELS, data, onSuccess, onError)
+    }
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormHeader
           title='Channel'
           subtitle='Manage a place where you and your friends hangout'
         />
         <div className='mt-1'>
-          <UncontrolledInput label='Name' type={'text'} />
+          <ControlledInput
+            label='Name'
+            value={channel}
+            onValueChange={setChannel}
+            validate={() => validateChannel(channel, error)}
+          />
         </div>
         <FormButton text='Create' />
       </form>
