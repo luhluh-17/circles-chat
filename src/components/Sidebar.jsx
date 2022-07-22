2c43790505591c6f9db04bd58037c8e6f58b16f1
luhluh-17 <castro.ralph17@gmail.com>
2022-07-23 01:12:14 +0800

    make sidebar dynamic component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,43 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import ModalAddChannel from '../parts/Channel/ModalAddChannel'
-import { apiGET } from '../services/api'
-import { CHANNELS } from '../services/constant'
+import React from 'react'
 import SidebarHeader from './SidebarHeader'
 import SidebarItems from './SidebarItems'
 
-function Sidebar() {
-  const [response, setResponse] = useState(null)
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggleDialog = () => setIsOpen(bool => !bool)
-  const closeDialog = () => setIsOpen(false)
-
-  const handleAdd = () => {
-    toggleDialog()
-  }
-
-  const handleSuccess = response => {
-    setResponse(response)
-  }
-
-  const handleError = message => {
-    console.log(message)
-  }
-
-  useEffect(() => {
-    const subscribeAPI = setInterval(() => {
-      apiGET(CHANNELS, handleSuccess, handleError)
-    }, 3000)
-    return () => {
-      clearInterval(subscribeAPI)
-    }
-  }, [])
-
+function Sidebar({ icon, title, response, onAdd }) {
   return (
     <aside className='sidebar'>
-      <SidebarHeader icon='diversity_3' title='Channels' onAdd={handleAdd} />
+      <SidebarHeader icon={icon} title={title} onAdd={onAdd} />
       <SidebarItems list={response?.data} errors={response?.errors} />
-      <ModalAddChannel isOpen={isOpen} onClose={closeDialog} />
     </aside>
   )
 }
--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+import ModalAddChannel from '../parts/Channel/ModalAddChannel'
+import { apiGET } from '../services/api'
+import { CHANNELS } from '../services/constant'
 
 function Channel() {
+  const [response, setResponse] = useState(null)
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleDialog = () => setIsOpen(bool => !bool)
+  const closeDialog = () => setIsOpen(false)
+  const handleAdd = () => toggleDialog()
+
+  const handleSuccess = response => {
+    setResponse(response)
+  }
+
+  const handleError = message => {
+    console.log(message)
+  }
+
+  useEffect(() => {
+    const subscribeAPI = setInterval(() => {
+      apiGET(CHANNELS, handleSuccess, handleError)
+    }, 3000)
+    return () => {
+      clearInterval(subscribeAPI)
+    }
+  }, [])
+
   return (
-    <section className='app-container'>
-      <Navbar />
-      <section className='app-content'>
-        <Sidebar />
-        <Outlet />
+    <>
+      <section className='app-container'>
+        <Navbar />
+        <section className='app-content'>
+          <Sidebar
+            icon='diversity_3'
+            title='Channels'
+            response={response}
+            onAdd={handleAdd}
+          />
+          <Outlet />
+        </section>
       </section>
-    </section>
+      <ModalAddChannel isOpen={isOpen} onClose={closeDialog} />
+    </>
   )
 }
 
